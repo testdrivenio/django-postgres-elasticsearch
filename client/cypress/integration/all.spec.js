3b58720519fd2339071985d776b64ac54d71cac0
Jason Parent <jason.a.parent@gmail.com>
2020-11-22 22:06:07 -0500

    Add client

---
+++ client/cypress/integration/all.spec.js
@@ -0,0 +1,33 @@
+describe('Perusable', function () {
+  it('Displays the home page.', function () {
+    cy.visit('/');
+    cy.get('h1').should('contain', 'Perusable');
+  });
+
+  it('Displays a list of results.', function () {
+    cy.server();
+    cy.route('GET', '**/api/v1/catalog/wines/**')
+      .as('getWines');
+  
+    cy.visit('/');
+  
+    cy.get('input[placeholder="Enter a search term (e.g. cabernet)"]')
+      .type('cabernet');
+  
+    cy.get('button').contains('Search').click();
+    cy.wait('@getWines');
+    cy.get('div.card-title').should('contain', 'Cabernet Sauvignon');
+  });
+  
+  it('Displays wine search words.', function () {
+    cy.server();
+    cy.route('GET', '**/api/v1/catalog/wine-search-words/**')
+      .as('getWineSearchWords');
+  
+    cy.visit('/');
+    cy.get('input[placeholder="Enter a search term (e.g. cabernet)"]')
+      .type('cabarnet');
+    cy.wait('@getWineSearchWords');
+    cy.get('div#query').should('contain', 'cabernet');
+  });
+});
